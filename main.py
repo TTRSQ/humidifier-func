b5854be18e71b833f48cc37cd5d2b9d11a210a28
TTRSQ <tst.ttrsq.123@gmail.com>
2023-01-11 02:41:42 +0900

    fix

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import src.switchbot as bot
+import functions_framework
+import src.usecase as api
+import os
 
-devices = bot.request_bot_api("/v1.1/devices", bot.Method.GET)["body"]["deviceList"]
-print(devices)
+plug_id = os.getenv("DEVICE_ID_PLUG")
+meter_id = os.getenv("DEVICE_ID_METER")
+humidifier_remote_id = os.getenv("REMOTE_ID_HUMIDIFIER")
+
+
+@functions_framework.http
+def hello_http(request):
+    plug_status = api.get_plug_status(plug_id)
+    if not plug_status["on"]:
+        return "ok"
+
+    is_humidifier_on = plug_status["weight"] > 10
+
+    meter_status = api.get_meter_status(meter_id)
+
+    if meter_status["humidity"] < 40 and not is_humidifier_on:
+        api.send_remote_command(humidifier_remote_id, "電源")
+
+    if meter_status["humidity"] >= 60 and is_humidifier_on:
+        api.send_remote_command(humidifier_remote_id, "電源")
+
+    return "ok"
---
+++ src/usecase.py
@@ -0,0 +1,61 @@
+import src.adapter.switchbot as bot
+
+
+def set_plug_power(plug_id: str, on: bool):
+    res = bot.request_bot_api(
+        f"/v1.1/devices/{plug_id}/commands",
+        bot.Method.POST,
+        {
+            "commandType": "command",
+            "command": "turnOn" if on else "turnOff",
+            "parameter": "default",
+        },
+    )
+    return {
+        "success": res["statusCode"] == 100,
+        "message": res["message"],
+    }
+
+
+def get_plug_status(plug_id: str):
+    res = bot.request_bot_api(
+        f"/v1.1/devices/{plug_id}/status",
+        bot.Method.GET,
+    )
+    return {
+        "success": res["statusCode"] == 100,
+        "message": res["message"],
+        "on": res["body"]["power"] == "on",
+        "voltage": res["body"]["voltage"],
+        "weight": res["body"]["weight"],
+    }
+
+
+def send_remote_command(remote_id: str, btn_name: str):
+    res = bot.request_bot_api(
+        f"/v1.1/devices/{remote_id}/commands",
+        bot.Method.POST,
+        {
+            "commandType": "customize",
+            "command": btn_name,
+            "parameter": "default",
+        },
+    )
+    return {
+        "success": res["statusCode"] == 100,
+        "message": res["message"],
+    }
+
+
+def get_meter_status(meter_id: str):
+    res = bot.request_bot_api(
+        f"/v1.1/devices/{meter_id}/status",
+        bot.Method.GET,
+    )
+    print(res)
+    return {
+        "success": res["statusCode"] == 100,
+        "message": res["message"],
+        "humidity": res["body"]["humidity"],
+        "temperature": res["body"]["temperature"],
+    }
